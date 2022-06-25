22c69537169ffd7079509ce25c93ab1e5f6316d4
Stephen Leander <stephen.leander1@gmail.com>
2022-06-25 21:31:26 +0200

    Import About

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
+import About from './components/About';
 import './styles/main.css';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Hero />
       <Navbar />
+      <About />
     </div>
   );
 }
