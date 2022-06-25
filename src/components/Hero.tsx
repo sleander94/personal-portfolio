afb5503b65593b6d25a5cb2a4f61cf78d48a7627
Stephen Leander <stephen.leander1@gmail.com>
2022-06-25 17:41:19 +0200

    Remove periods

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,9 +2,9 @@ const Hero = () => {
   return (
     <header>
       <div className="textOne">
-        Hi, I'm <span>Stephen</span>.
+        Hi, I'm <span>Stephen</span>
       </div>
-      <div className="textTwo">Welcome to my portfolio.</div>
+      <div className="textTwo">Welcome to my portfolio</div>
       <button type="button">↓</button>
     </header>
   );
