c69701adfebeb270f8b75d64424aed50826d3ec6
Stephen Leander <stephen.leander1@gmail.com>
2022-06-25 18:09:18 +0200

    Import Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Hero from './components/Hero';
+import Navbar from './components/Navbar';
 import './styles/main.css';
 
 function App() {
   return (
     <div className="App">
       <Hero />
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <a href="#home" className="active">
+        Home
+      </a>
+      <a href="#about">About</a>
+      <a href="#projects">Projects</a>
+      <a href="#contact">Contact</a>
+    </nav>
+  );
+};
+
+export default Navbar;
