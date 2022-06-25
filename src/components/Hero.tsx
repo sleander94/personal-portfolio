7855510565e6ebbc5d12793b3f1a10d095f34630
Stephen Leander <stephen.leander1@gmail.com>
2022-06-25 17:14:24 +0200

    Initial Hero commit

---
+++ src/components/Hero.tsx
@@ -0,0 +1,13 @@
+const Hero = () => {
+  return (
+    <header>
+      <div className="textOne">
+        Hi, I'm <span>Stephen</span>.
+      </div>
+      <div className="textTwo">Welcome to my portfolio.</div>
+      <button type="button">↓</button>
+    </header>
+  );
+};
+
+export default Hero;
