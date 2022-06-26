305742806aed701a83543445f55c396fbae10920
Stephen Leander <stephen.leander1@gmail.com>
2022-06-26 12:36:17 +0200

    Change header to section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,12 +1,12 @@
 const Hero = () => {
   return (
-    <header id="home">
+    <section id="home">
       <div className="textOne">
         Hi, I'm <span>Stephen</span>
       </div>
       <div className="textTwo">Welcome to my portfolio</div>
       <a href="#about">↓</a>
-    </header>
+    </section>
   );
 };
 
