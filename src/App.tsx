592080425d803289711c7afcb5ebf6ed4bc3b0c7
Stephen Leander <stephen.leander1@gmail.com>
2022-07-19 16:24:20 -0600

    Move window scroll event handler to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
+import { useEffect } from 'react';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 import About from './components/About';
 import Projects from './components/Projects';
 import './styles/main.css';
 
-function App() {
+const App = () => {
+  useEffect(() => {
+    let sections = document.querySelectorAll('section');
+    let menu = document.querySelectorAll('nav a');
+    const handleScroll = () => {
+      sections.forEach((section) => {
+        let top = window.scrollY;
+        let offset = section.offsetTop - 54;
+        let height = section.offsetHeight;
+        let id = section.getAttribute('id');
+        if (top >= offset && top < offset + height) {
+          menu.forEach((link) => {
+            link.classList.remove('active');
+            document
+              .querySelector('nav a[href*=' + id + ']')!
+              .classList.add('active');
+          });
+        }
+      });
+    };
+    window.addEventListener('scroll', handleScroll);
+  }, []);
+
   return (
     <div className="App">
       <Hero />
@@ -13,6 +36,6 @@ function App() {
       <Projects />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,24 +1,4 @@
 const Navbar = () => {
-  let sections = document.querySelectorAll('section');
-  let menu = document.querySelectorAll('nav a');
-  window.onscroll = () => {
-    sections.forEach((section) => {
-      let top = window.scrollY;
-      let offset = section.offsetTop - 54;
-      let height = section.offsetHeight;
-      let id = section.getAttribute('id');
-      if (top >= offset && top < offset + height) {
-        menu.forEach((link) => {
-          link.classList.remove('active');
-
-          document
-            .querySelector('nav a[href*=' + id + ']')!
-            .classList.add('active');
-        });
-      }
-    });
-  };
-
   return (
     <nav>
       <a href="#home" className="active">
