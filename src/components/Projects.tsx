925196e9afe2c748101a555a8e9a8564dcc516af
Stephen Leander <stephen.leander1@gmail.com>
2022-07-20 15:58:41 -0600

    Edit project descriptions and links

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -11,12 +11,28 @@ const Projects = () => {
       <div className="project-container">
         <ProjectCard
           title="Park At A Glance"
-          description="Fullstack app displaying important information about America's
-          National Parks in one place."
+          description="Front-End app that uses multiple APIs to display important information about America's
+          National Parks in one place. Built with React, Typescript, and Tailwind"
           site="parkataglance.com"
           repo="github.com/sleander94/park-at-a-glance"
-          image="parkataglance.png"
-          altText="parkataglance"
+          image="parkataglance.gif"
+          altText="park at a glance demo"
+        />
+        <ProjectCard
+          title="Hunt for Legends"
+          description="Full-Stack photo tagging app with leaderboards. Built with React and Firebase."
+          site="hunt-for-legends-3f7c3.web.app/home"
+          repo="github.com/sleander94/hunt-for-legends"
+          image="huntforlegends.gif"
+          altText="hunt for legends demo"
+        />
+        <ProjectCard
+          title="CV Generator"
+          description="Front-End app that converts user input into downloadable PDF. Built using React"
+          site="sleander94.github.io/cv-app/"
+          repo="github.com/sleander94/cv-app"
+          image="cvgenerator.gif"
+          altText="CV generator demo"
         />
       </div>
     </section>
