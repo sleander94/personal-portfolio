d1a4172c9f953b9b9271ad77a92628f8c7efaef3
Stephen Leander <stephen.leander1@gmail.com>
2022-08-04 21:44:47 -0600

    Add we spy project card

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import ProjectCard from './ProjectCard';
 
 const Projects = () => {
@@ -10,8 +11,16 @@ const Projects = () => {
       </div>
       <div className="project-container">
         <ProjectCard
+          title="We Spy"
+          description="Full Stack app allowing users to built and share their own photo hunt games. Build with React, Typescript, and Firebase"
+          site="wespy.app"
+          repo="https://github.com/Scott-Coates-Org/solo-project-sleander94"
+          image="wespy.gif"
+          altText="we spy demo"
+        />
+        <ProjectCard
           title="Park At A Glance"
-          description="Front-End app that uses multiple APIs to display important information about America's
+          description="Front End app that uses multiple APIs to display important information about America's
           National Parks in one place. Built with React, Typescript, and Tailwind"
           site="parkataglance.com"
           repo="github.com/sleander94/park-at-a-glance"
@@ -20,7 +29,7 @@ const Projects = () => {
         />
         <ProjectCard
           title="Hunt for Legends"
-          description="Full-Stack photo tagging app with leaderboards. Built with React and Firebase."
+          description="Full Stack photo tagging app with leaderboards. Built with React and Firebase."
           site="hunt-for-legends-3f7c3.web.app/home"
           repo="github.com/sleander94/hunt-for-legends"
           image="huntforlegends.gif"
@@ -28,7 +37,7 @@ const Projects = () => {
         />
         <ProjectCard
           title="CV Generator"
-          description="Front-End app that converts user input into downloadable PDF. Built using React"
+          description="Front End app that converts user input into downloadable PDF. Built using React"
           site="sleander94.github.io/cv-app/"
           repo="github.com/sleander94/cv-app"
           image="cvgenerator.gif"
