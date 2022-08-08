fb806481708481dff4328b1557783803b7e97f7b
Stephen Leander <stephen.leander1@gmail.com>
2022-08-07 21:28:31 -0600

    Update props, render tech / features arrays & mobile image

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,38 +1,63 @@
 type CardProps = {
   title: string;
+  technologies: string[];
   description: string;
+  features: string[];
   site: string;
   repo: string;
-  image: string;
+  desktopImage: string;
+  mobileImage: string;
   altText: string;
 };
 
 const ProjectCard = ({
   title,
+  technologies,
   description,
+  features,
   site,
   repo,
-  image,
+  desktopImage,
+  mobileImage,
   altText,
 }: CardProps) => {
-  const path = require(`../assets/projects/${image}`);
-
   return (
     <div className="project">
       <div className="info">
         <h2>{title}</h2>
+        <ul className="technologies">
+          {technologies.map((tech) => {
+            return <li key={technologies.indexOf(tech)}>{tech}</li>;
+          })}
+        </ul>
         <p>{description}</p>
+        <ul className="features">
+          {features.map((feature) => {
+            return <li key={features.indexOf(feature)}>{feature}</li>;
+          })}
+        </ul>
         <div className="links">
           <a href={`https://${site}`} target="_blank">
-            Site
+            Live
           </a>
           <a href={`https://${repo}`} target="_blank">
             Code
           </a>
         </div>
       </div>
-      <div className="project-image">
-        <img src={path} alt={altText}></img>
+      <div className="preview-images">
+        <div className="image-container">
+          <img
+            className="desktop"
+            src={require(`../assets/projects/${desktopImage}`)}
+            alt={altText + 'desktop'}
+          ></img>
+          <img
+            className="mobile"
+            src={require(`../assets/projects/${mobileImage}`)}
+            alt={altText + 'mobile'}
+          ></img>
+        </div>
       </div>
     </div>
   );
