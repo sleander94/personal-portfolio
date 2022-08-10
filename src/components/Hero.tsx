1eb7aa6bcd775b7086e67ab8ca90e94d2b02339f
Stephen Leander <stephen.leander1@gmail.com>
2022-08-10 12:01:06 -0600

    Update hero animation and styles

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,11 +1,21 @@
 const Hero = () => {
   return (
     <section id="home">
-      <div className="text-one">
+      <div className="hero-media">
+        <img
+          src={require('../assets/icons/android-chrome-512x512.png')}
+          alt="SL initials icon"
+        />
+        <div className="heading-container">
+          <h1>Stephen Leander</h1>
+        </div>
+        <a href="#about">{'>'}</a>
+      </div>
+      {/*       <div className="text-one">
         Hi, I'm <span>Stephen</span>
       </div>
       <div className="text-two">Welcome to my portfolio</div>
-      <a href="#about">↓</a>
+      <a href="#about">↓</a> */}
     </section>
   );
 };
